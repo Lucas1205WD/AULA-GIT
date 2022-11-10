7162884ad692e26dc5d02cf7381507778678d63e
Lucas <lucas.oliveira@duque.g12.br>
2022-11-10 20:56:31 -0300

    exibir com data e hora

---
+++ text.py
@@ -0,0 +1,10 @@
+nome =  'Lucas'
+idade = 17
+
+print(f'EU sou {nome} tenho {idade} anos')
+
+idiomas = ['espanhol', 'portugues']
+
+print(f'Eu falo {idiomas[0]}')
+
+print('Sapiranga, 10 de novembro')
